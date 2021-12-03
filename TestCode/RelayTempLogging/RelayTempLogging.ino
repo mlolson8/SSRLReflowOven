de303cd18fcb1d919e3309b1cb467f74f75619e8
mlolson8 <mlo51002@uga.edu>
2021-12-03 13:36:59 -0500

    100% Duty Cyccle Test and Started augmenting code to account for Duty Cycle

--- TestCode/RelayTempLogging/RelayTempLogging.ino
+++ TestCode/RelayTempLogging/RelayTempLogging.ino
@@ -18,7 +18,8 @@ int OvenCount = 0;
 int DispCount = 0;
 int8_t CS0_PIN = 10;
 PWFusion_MAX31855_TC  thermocouple0(CS0_PIN);
-unsigned long myTime;
+unsigned long previousMillis = 0;
+const long SampleInterval = 500;
 
 
 void setup()
@@ -39,56 +40,54 @@ void setup()
 
 void loop()
 {
-  //delay(500);                                   // 500ms delay... can be much faster
   //Serial.print("Time: ");
-  myTime = millis();
-  float Seconds = (float) myTime/1000;
-  DispCount ++;
-  if(DispCount > 10000){ //Reduces the number of read datapoints so its doesn't spam the serial monitor
-  Serial.print(String(Seconds) + "\t"); // prints time since program started
-  //delay(1000);          // wait a second so as not to send massive amounts of data
-  
-  static struct var_max31855 TC_CH0 = {0, 0, 0, 3, 0}; //Specifies T-Type
-  double tmp;
-  struct var_max31855 *tc_ptr;
-  
-  // update T.C. Reading
-  tc_ptr = &TC_CH0;
-  thermocouple0.MAX31855_update(tc_ptr);        // Update MAX31855 readings   
-
-  //Temp Reading
-  tmp = (double)TC_CH0.ref_jcn_temp * 0.0625;  // convert fixed pt # to double
-  if((-100 > tmp) || (150 < tmp)){Serial.println("unknown fault");}
-  tmp = (double)TC_CH0.value * 0.25;           // convert fixed pt # toouble
-  //Serial.print("Temp = ");                     // print TC temp heading
-  if(0x00 == TC_CH0.status){Serial.println(tmp);}
-  else if(0x01 == TC_CH0.status){Serial.println("OPEN");}
-  else if(0x02 == TC_CH0.status){Serial.println("SHORT TO GND");}
-  else if(0x04 == TC_CH0.status){Serial.println("SHORT TO Vcc");}
-  else{Serial.println("unknown fault");
-  DispCount = 0;
+  unsigned long currentMillis = millis();
+  float Seconds = (float) currentMillis/1000;
+   
+  if(currentMillis - previousMillis >= SampleInterval){ //Reduces the number of read datapoints so its doesn't spam the serial monitor
+    previousMillis = currentMillis;
+    Serial.print(String(Seconds) + "\t"); // prints time since program started
+    static struct var_max31855 TC_CH0 = {0, 0, 0, 3, 0}; //Specifies T-Type
+    double tmp;
+    struct var_max31855 *tc_ptr;
+    // update T.C. Reading
+    tc_ptr = &TC_CH0;
+    thermocouple0.MAX31855_update(tc_ptr);        // Update MAX31855 readings   
+    //Temp Reading
+    tmp = (double)TC_CH0.ref_jcn_temp * 0.0625;  // convert fixed pt # to double
+    if((-100 > tmp) || (150 < tmp)){Serial.println("unknown fault");}
+    tmp = (double)TC_CH0.value * 0.25;           // convert fixed pt # toouble
+    //Serial.print("Temp = ");                     // print TC temp heading
+    if(0x00 == TC_CH0.status){Serial.println(tmp);}
+    else if(0x01 == TC_CH0.status){Serial.println("OPEN");}
+    else if(0x02 == TC_CH0.status){Serial.println("SHORT TO GND");}
+    else if(0x04 == TC_CH0.status){Serial.println("SHORT TO Vcc");}
+    else{Serial.println("unknown fault");
   }
  }
 
   
-//Push Button Controls
- cVal_old = cVal;
- delay(1);
- cVal = digitalRead(pinC);
-if((cVal != cVal_old) && (cVal == 1)){
-  Serial.println("Button pushed");
-  //delay(500);
-  FanCount++;
-  Serial.println(FanCount);
- }
-if (FanCount % 2 == 1) {
-  //Serial.println("Fan ON");
-  digitalWrite(PWR_Relay, HIGH);
-  digitalWrite(Fan_Relay, HIGH);
-}
-else if (FanCount % 2 == 0){
-  //Serial.println("Fan OFF");
-  digitalWrite(PWR_Relay, LOW);
-  digitalWrite(Fan_Relay, LOW);
-}
+  //Relay Push Button Controls
+   cVal_old = cVal;
+   delay(1);
+   cVal = digitalRead(pinC);
+  if((cVal != cVal_old) && (cVal == 1)){
+    FanCount++;
+    //Serial.println("Button pushed:" + String(Fancount));
+   }
+  if (FanCount % 3 == 0) {
+    //Serial.println("Fan OFF;Heat OFF");
+    digitalWrite(PWR_Relay, LOW);
+    digitalWrite(Fan_Relay, LOW);
+  }
+  else if (FanCount % 3 == 1){
+    //Serial.println("Fan ON;Heat ON");
+    digitalWrite(PWR_Relay, HIGH);
+    digitalWrite(Fan_Relay, HIGH);
+  }
+   else if (FanCount % 3 == 2){
+    //Serial.println("Fan ON;Heat OFF");
+    digitalWrite(PWR_Relay, LOW);
+    digitalWrite(Fan_Relay, HIGH);
+  }
 }
